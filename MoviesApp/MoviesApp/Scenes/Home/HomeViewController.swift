9adf685b139429e2dec1561165d6988a167ceddf
Omnia Samy <omnia.samy@Omnias-MacBook-Pro.local>
2020-09-09 15:39:17 +0200

    add movie app using mvvm

--- MoviesApp/MoviesApp/Scenes/Home/HomeViewController.swift
+++ MoviesApp/MoviesApp/Scenes/Home/HomeViewController.swift
@@ -18,6 +18,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.title = "Movies"
         observe()
         
         homeTableView.dataSource = self
